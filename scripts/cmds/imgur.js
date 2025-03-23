876a7387cdd1e680fbeac36f25c85ac052b39541
samilazo <sancamellia91@gmail.com>
2025-03-23 05:43:47 +0100

    Update imgur.js

--- scripts/cmds/imgur.js
+++ scripts/cmds/imgur.js
@@ -1,37 +1,41 @@
-const axios = require('axios');
+ const axios = require('axios');
 
-module.exports = {
- config: {
- name: "imgur",
- version: "1.0",
- author: "ArYAN",
- countDown: 5,
- role: 0,
- shortDescription: {
- en: "Upload image to imbb"
- },
- longDescription: {
- en: "Upload image to imbb by replying to photo"
- },
- category: "tools",
- guide: {
- en: ""
- }
- },
+module.exports.config = {
+  name: "imgur",
+  aliases: ["i"],
+  version: "3.0",
+  author: "Lawkey Marvellous",
+  countDown: 5,
+  role: 0,
+  category: "tools",
+  description: "Upload media to get GoatBin link.",
+  usages: "Reply to a media file (image, video, etc.)",
+};
 
- onStart: async function ({ api, event }) {
- const linkanh = event.messageReply?.attachments[0]?.url;
- if (!linkanh) {
- return api.sendMessage('Please reply to an image.', event.threadID, event.messageID);
- }
+module.exports.onStart = async function ({ api, event }) {
+  const baseApiUrl = "https://goatbiin.onrender.com/v1";
+  const attachments = event.messageReply?.attachments;
 
- try {
- const res = await axios.get(`https://aryan-noobs-apis.onrender.com/imgur?link=${encodeURIComponent(linkanh)}`);
- const juswa = res.data.uploaded.image;
- return api.sendMessage(juswa, event.threadID, event.messageID);
- } catch (error) {
- console.log(error);
- return api.sendMessage('Failed to upload image to imbb.', event.threadID, event.messageID);
- }
- }
-}
+  if (attachments && attachments.length > 0) {
+    try {
+      const mediaLinks = [];
+
+      for (const attachment of attachments) {
+        if (attachment.url) {
+          const { data } = await axios.post(`${baseApiUrl}/upload`, { url: attachment.url });
+          if (data.link) mediaLinks.push(data.link);
+        }
+      }
+
+      if (mediaLinks.length > 0) {
+        return api.sendMessage(`${mediaLinks.join("\n")}`, event.threadID, event.messageID);
+      } else {
+        return api.sendMessage("Failed to upload media.", event.threadID, event.messageID);
+      }
+    } catch (err) {
+      return api.sendMessage(`Media Upload Error: ${err.message}`, event.threadID, event.messageID);
+    }
+  }
+
+  return api.sendMessage("Please reply to a media file (image, video, etc.) to upload it to GoatBin.", event.threadID, event.messageID);
+};
